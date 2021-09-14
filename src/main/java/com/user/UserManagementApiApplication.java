d302e17c79d3cfddea820f1ad2d2417e5aac4b7c
Omkar Warghade <sid.mangesh@gmail.com>
2021-09-14 17:27:32 +0530

    package renamed

--- src/main/java/com/user/UserManagementApiApplication.java
+++ src/main/java/com/user/UserManagementApiApplication.java
@@ -1,4 +1,4 @@
-package com.user.usermanagementapi;
+package com.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserManagementApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserManagementApiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(UserManagementApiApplication.class, args);
+    }
 }
--- src/test/java/com/user/UserManagementApiApplicationTests.java
+++ src/test/java/com/user/UserManagementApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.user.usermanagementapi;
+package com.user;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
